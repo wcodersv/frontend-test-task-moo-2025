078c5f522438e5a1bdc8d938d925fc79ef258277
Viktoriia <viktoriasamo.s@gmail.com>
2025-02-28 14:42:08 +0400

    task-1 - a couple of fixes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,5 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../app/App";
-// import ErrorPage from "../pages/ErrorPage";
 import { SignIn} from "../pages/sign-in";
 import {AboutUs} from "../pages/about-us";
 import {Profile} from "../pages/profile";
@@ -10,7 +9,6 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    // errorElement: <ErrorPage />,
     children: [
       { path: "", element: <AboutUs /> },
       {
